09c6ab7e7fb6f06de0ec20060d7d0a77f7b352b3
tiffanibutarbutar <tiffanibutarbutar08@gmail.com>
2024-02-06 21:39:38 +0700

    t03.02

--- t03_02.c
+++ t03_02.c
@@ -4,8 +4,34 @@
 
 int main(int _argc, char **_argv)
 {
+  int n, max, min, total = 0, average;
 
-  // your code here
+  scanf("%d", &n);
 
+  int num[n];
+  for (int i = 0; i < n; i++)
+  { 
+    scanf("%d", &num[i]);
+    total += num[i];
+  }
+  
+  max = num[0];
+  min = num[0];
+  for (int i = 0; i < n; i++)
+  {
+    if (num[i] > max)
+    {
+      max = num[i];
+    }
+    if (num[i] < min)
+    {
+      min = num[i];
+    }
+  }
+  
+  average = total / n;  
+  printf("%d\n", min);
+  printf("%d\n", max);
+  printf("%d\n", average);
   return 0;
 }
