d1486493e5c6fc10986283e2f06e7a6fa3800feb
tiffanibutarbutar <tiffanibutarbutar08@gmail.com>
2024-02-06 21:14:47 +0700

    t03_01

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,34 @@
-// NIM - NAMA
+// 12S23010 - Tiffani Rumiris Br Butar-Butar
 
 #include <stdio.h>
 
 int main(int _argc, char **_argv)
 {
+  int n, max, min;
 
-  // your code here
+   scanf("%d", &n);
+
+  int num[n];
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", &num[i]);
+  }
+  
+  max = num[0];
+  min = num[0];
+  for (int i = 0; i < n; i++)
+  {
+    if (num[i] > max)
+    {
+      max = num[i];
+    }
+    if (num[i] < min)
+    {
+      min = num[i];
+    }
+  }
+  printf("%d\n", min);
+  printf("%d\n", max);
 
   return 0;
 }
